0fd6a79f1b32de993c4d46f3caa7792a068af3db
Yasharth <nothing>
2020-05-14 17:09:24 +0530

    Uploading the FinalApp's initial work

---
+++ FinalApp/FinalAssignmentApp/Model/CountryModel.swift
@@ -0,0 +1,14 @@
+//
+//  CountryModel.swift
+//  FinalAssignmentApp
+//
+//  Created by Yasharth Saxena on 14/05/20.
+//  Copyright © 2020 Yasharth Saxena. All rights reserved.
+//
+
+import Foundation
+
+
+struct CountryModel: Decodable {
+    let name: String
+}
